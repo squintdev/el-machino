0ea282317953c94aa722a195a43153b7d0cd58ae
Tom Duffy <thomas.edward.duffy@gmail.com>
2021-12-06 19:24:43 -0800

    installed tailwind and setup basic routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+import Parametric from './components/Parametric';
+import Home from './components/Home';
+import Fractal from './components/Fractal';
+import Motion from './components/Motion';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/parametric" element={<Parametric />} />
+      <Route path="/fractal" element={<Fractal />} />
+      <Route path="/motion" element={<Motion />} />
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
